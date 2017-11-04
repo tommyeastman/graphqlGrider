035cfa47dbd2ab348559c2d7675bcdf0b5d763d1
tommyeastman1 <tommyeastman1@gmail.com>
2017-11-04 00:00:42 -0300

    apollo client, mongoDB, express, webpack, and graphql setup

--- client/index.js
+++ client/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-client";
+//Apollo Provider is the glue layer between apollo and react and is a react component
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <div>Lyrical</div>
+    </ApolloProvider>
+  );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector("#root"));
